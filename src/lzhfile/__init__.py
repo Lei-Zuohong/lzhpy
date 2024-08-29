cf49340d7286a20773da0d36ecb40ca30acad5a0
Leizuohong <leizuoho@mail.ustc.edu.cn>
2024-08-29 16:36:55 +0800

    123

--- src/lzhfile/__init__.py
+++ src/lzhfile/__init__.py
@@ -3,8 +3,7 @@
 # Private package
 # Internal package
 from .core import *
-from .types.math import *
 from .types.txt import *
 from .types.pkl import *
-from .types.ini import *
 from .types.json import *
+from .types.math import *
